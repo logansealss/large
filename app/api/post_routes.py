045d7ea2c045e99cd646a3fb968211689ff21b3f
Richard Logan Seals <richard.logan.seals@gmail.com>
2022-10-26 01:37:20 -0500

    added update and delete routes for posts

--- app/api/post_routes.py
+++ app/api/post_routes.py
@@ -1,12 +1,19 @@
+from turtle import pos
 from flask import Blueprint, request
 from sqlalchemy.orm import joinedload
 from flask_login import current_user, login_required
 
 from app.models import db, Post, Response, Clap
-from app.forms.post_form import PostForm
+from app.forms.post_form import CreatePostForm, UpdatePostForm
 from app.api.auth_routes import validation_errors_to_error_messages
 
 AVG_READING_SPEED = 250
+
+def calculate_read_time(number_of_words):
+    return round(number_of_words / AVG_READING_SPEED)
+
+def get_word_count(string):
+    return len(string.split(' '))
 
 post_routes = Blueprint("post", __name__)
 
@@ -35,7 +42,8 @@ def get_post_by_id(post_id):
     if post_by_id is None:
         return {
             "message": "Post couldn't be found",
-            "statusCode": 404}, 404
+            "statusCode": 404
+        }, 404
 
     num_claps = Post.query.join(Clap)           \
         .filter(Post.id == post_id)             \
@@ -51,16 +59,18 @@ def get_post_by_id(post_id):
 
     return post_dict
 
-# -------------- CREATE POST -------------- #
+# -------------- CREATE A POST -------------- #
 
 @post_routes.route('', methods=["POST"])
+@login_required
 def create_post():
-    form = PostForm()
+    form = CreatePostForm()
     form['csrf_token'].data = request.cookies['csrf_token']
 
     if form.validate_on_submit():
         form_data = form.data
-        read_time = len(form_data["post"].split(' ')) // AVG_READING_SPEED
+        word_count = get_word_count(form_data["post"])
+        read_time = calculate_read_time(word_count)
         new_post = Post(writer_id=current_user.id,
                         read_time=read_time,
                         title=form_data["title"],
@@ -71,6 +81,79 @@ def create_post():
         db.session.add(new_post)
         db.session.commit()
 
-        return new_post.to_dict()
+        return new_post.writer_to_dict()
 
     return {'errors': validation_errors_to_error_messages(form.errors)}, 400
+
+# -------------- UPDATE A POST -------------- #
+
+@post_routes.route('/<int:post_id>', methods=["PUT"])
+@login_required
+def update_post_by_id(post_id):
+    post_by_id = Post.query.get(post_id)
+
+    if post_by_id is None:
+        return {
+            "message": "Post couldn't be found",
+            "statusCode": 404
+        }, 404
+
+    if post_by_id.writer_id != current_user.id:
+        return {
+            "message": "Forbidden",
+            "statusCode": 403
+        }, 403
+
+    form = UpdatePostForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+
+    if form.validate_on_submit():
+        form_data = form.data
+
+        if form_data["title"]:
+            post_by_id.title = form_data["title"]
+
+        if form_data["subtitle"] is not None:
+            post_by_id.subtitle = form_data["subtitle"]
+
+        if form_data["post"]:
+            post_by_id.post = form_data["post"]
+            word_count = get_word_count(form_data["post"])
+            read_time = calculate_read_time(word_count)
+            post_by_id.read_time = read_time
+
+        if form_data["image_url"] is not None:
+            post_by_id.image_url = form_data["image_url"]
+
+        db.session.commit()
+
+        return post_by_id.writer_to_dict()
+
+    return {'errors': validation_errors_to_error_messages(form.errors)}, 400
+
+# -------------- DELETE A POST -------------- #
+
+@post_routes.route('/<int:post_id>', methods=["DELETE"])
+@login_required
+def delete_post_by_id(post_id):
+    post_by_id = Post.query.get(post_id)
+
+    if post_by_id is None:
+        return {
+            "message": "Post couldn't be found",
+            "statusCode": 404
+        }, 404
+
+    if post_by_id.writer_id != current_user.id:
+        return {
+            "message": "Forbidden",
+            "statusCode": 403
+        }, 403
+
+    db.session.delete(post_by_id)
+    db.session.commit()
+
+    return {
+        "message": "Successfully deleted",
+        "statusCode": 200
+    }
--- app/forms/post_form.py
+++ app/forms/post_form.py
@@ -4,8 +4,14 @@ from wtforms.validators import DataRequired, Length, regexp
 
 # regexp("(?i)([^\\s]+(\\.(jpe?g|png|gif|bmp))$)")
 
-class PostForm(FlaskForm):
+class CreatePostForm(FlaskForm):
     title = StringField('title', validators=[DataRequired(), Length(1, 100)])
     subtitle = StringField('subtitle', validators=[Length(0, 100)])
     image_url = StringField('image url', [Length(0, 255)])
     post = StringField('post', validators=[DataRequired()])
+
+class UpdatePostForm(FlaskForm):
+    title = StringField('title', validators=[Length(0, 100)])
+    subtitle = StringField('subtitle', validators=[Length(0, 100)])
+    image_url = StringField('image url', [Length(0, 255)])
+    post = StringField('post')
