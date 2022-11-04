3d37aa1a329f3992bc5a9541bfd1701ead75edd0
Richard Logan Seals <richard.logan.seals@gmail.com>
2022-11-04 16:29:26 -0500

    claps display and functionality in html/css

--- react-app/src/components/PostPage/PostFooterClaps.js
+++ react-app/src/components/PostPage/PostFooterClaps.js
@@ -1,5 +1,6 @@
 
 import { useSelector } from "react-redux"
+import { useState } from "react"
 
 import ReusableModal from "../../context/ReusableModal"
 import clap from "../../images/clap.svg"
@@ -7,15 +8,67 @@ import Claps from "./Claps/Claps"
 
 export default function PostFooterClaps() {
 
+    const user = useSelector(state => state.session.user)
     const claps = useSelector(state => state.claps)
-
     const numClaps = Object.values(claps).reduce((sum, cur) => sum += cur.amount, 0)
+    const [intObj, setIntObj] = useState()
+    const [timeoutObj, setTimeoutObj] = useState()
+    const [clapsAmount, setClapsAmount] = useState(0)
+    const [bubbleVisible, setBubbleVisible] = useState(false)
+
+    function bubbleDisappears(){
+        setBubbleVisible(false)
+        
+    }
+
+    function clapsCounter() {
+
+        if(clapsAmount < 50){
+            setClapsAmount(cur => cur + 1)
+        }
+    }
+
+    function clapsStopClick() {
+
+        if(intObj){
+            clearInterval(intObj)
+            setIntObj()
+        }
+        setTimeoutObj(setTimeout(bubbleDisappears, 750))
+    }
+
+    function clapsMouseDown() {
+        if(timeoutObj){
+            clearTimeout(timeoutObj)
+        }
+        if(clapsAmount < 50){
+            setClapsAmount(cur => cur + 1)
+            setIntObj(setInterval(clapsCounter, 500))
+        }
+        setBubbleVisible(true)
+    }
 
     return (
         <div id="post-footer-claps">
             <div id="post-footer-claps-flex">
-                <div className="svg-container svg-claps">
-                    <img src={clap} />
+                <div
+                    className="claps-container"
+                >
+                    <div
+                        className="svg-container svg-claps"
+                        onMouseDown={clapsMouseDown}
+                        onMouseUp={clapsStopClick}
+                        onDragEnd={clapsStopClick}
+                    >
+                        <img src={clap} />
+                    </div>
+                    <div
+                        className={bubbleVisible? "floating-claps visible" : "floating-claps hidden"}
+                    >
+                        <div>
+                            {clapsAmount <= 50 ? clapsAmount : 50}
+                        </div>
+                    </div>
                 </div>
                 {!!numClaps &&
                     (<ReusableModal
